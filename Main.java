074ba182b72c77dbe512a3c95e4f1a1ac96ed294
Mateusz Tarczyński <58234909+MrMT-95@users.noreply.github.com>
2020-10-08 21:29:14 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,54 @@
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
-
     public static void main(String[] args) {
-        boolean valid;
+        // write your code here
 
+        Scanner scanner = new Scanner(System.in);
 
-        do {
-            Scanner input = new Scanner(System.in);
-            System.out.println("Please provide your password:");
-            String password = input.nextLine();
-            passwordValidation checkPass = new passwordValidation();
+        ArrayList<Integer> arrayList = readArrayList(scanner);
+        int number = scanner.nextInt();
 
-            checkPass.check4Length(password);
+        int minValue = minDifference(arrayList, number);
+        ArrayList<Integer> output = outputArray(arrayList, number, minValue);
 
+        System.out.println(output.toString().replaceAll("\\[|\\]", "").replaceAll("\\,", ""));
 
-            checkPass.check4Number(password);
-
-            checkPass.check4Uppercase(password);
-
-            checkPass.check4Specialchar(password);
-
-            valid = checkPass.valid1 & checkPass.valid2 & checkPass.valid3 & checkPass.valid4;
-
-            if (valid == false) {
-                System.out.println("Wrong password!");
-            }
-
-
-        } while (!valid);
 
     }
+
+    private static ArrayList<Integer> readArrayList(Scanner scanner) {
+        return Arrays
+                .stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    private static int minDifference(ArrayList<Integer> arrayList, int number) {
+
+        int min;
+        ArrayList<Integer> minArrayList = new ArrayList<>();
+
+        for (Integer integer : arrayList) {
+            minArrayList.add(Math.abs(integer - number));
+        }
+        min = minArrayList.get(minArrayList.indexOf(Collections.min(minArrayList)));
+
+        return min;
+    }
+
+    private static ArrayList<Integer> outputArray(ArrayList<Integer> arrayList, int number, int minValue) {
+        ArrayList<Integer> outputArrayList = new ArrayList<>();
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (Math.abs(arrayList.get(i) - number) == minValue) {
+                outputArrayList.add(arrayList.get(i));
+            }
+        }
+        Collections.sort(outputArrayList);
+
+        return outputArrayList;
+    }
 }
+
